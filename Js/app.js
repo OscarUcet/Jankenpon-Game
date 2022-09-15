4f2067faf4550579347d4e49e42d8cd38ef05816
Oscar Uceta <104914253+OscarUcet@users.noreply.github.com>
2022-09-15 15:06:23 -0400

    change variable name

--- Js/app.js
+++ Js/app.js
@@ -5,20 +5,22 @@ const paper = document.getElementById('container-paper');
 const scissors = document.getElementById('container-scissors');
 const rules = document.getElementById('rules');
 
+const game = new Game(0);
 // button rock
 rock.addEventListener('click', ()=> {
     console.log('My Choose: Rock #0')
-    const juego = new Game(0);
+    // const game = new Game(0);
+    game.setChoose(0);
 });
 // button paper
 paper.addEventListener('click', ()=> {
     console.log('My Choose: Paper #1')
-    const juego = new Game(1);
+    game.setChoose(1);
 });
 // button scissors
 scissors.addEventListener('click', ()=> {
     console.log('My Choose: Scissors #2')
-    const juego = new Game(2);
+    game.setChoose(2);
 });
 // button rules
 rules.addEventListener('click', ()=> {
